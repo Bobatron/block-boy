65e8412bc666a4e00e1393ea97ccfcd54a3b01f0
Bob Love <bob@niaxo.co>
2025-04-10 23:12:32 +0100

    adding menu

--- code/main.js
+++ code/main.js
@@ -20,6 +20,7 @@ var maxBlocks = 5;
 var blockSize = 52;
 var gridSize = 50;
 var blockManager;
+var gameState;
 
 function setup() {
     createCanvas(width, height);
@@ -28,9 +29,21 @@ function setup() {
     grid = new Grid(width, height, gridSize);
     character = new Character();
     blockManager = new BlockManager(blockSize, maxBlocks, gridSize);
+    gameState = 'menu';
 }
 
 function draw() {
+    switch (gameState) {
+        case 'menu':
+            drawMenuScreen();
+            break;
+        case 'gameplay':
+            gameplay();
+            break;
+    }
+}
+
+function gameplay() {
     background(51);
     text(frameRate(), 10, 10);
     //text(a, 30, 30);
@@ -41,19 +54,19 @@ function draw() {
     for (var i = 0; i < blockManager.blocks.length; i++) {
         // blocks[i].draw();
         var collisionXPadding = 5;
-        if(!character.onGround && collideLineRect(blockManager.blocks[i].x + collisionXPadding, blockManager.blocks[i].y + 5, blockManager.blocks[i].x + blockManager.blocks[i].dimension - collisionXPadding, blockManager.blocks[i].y + 5, character.x + (character.width/3), character.y + character.height, character.width/3, 5)){
+        if (!character.onGround && collideLineRect(blockManager.blocks[i].x + collisionXPadding, blockManager.blocks[i].y + 5, blockManager.blocks[i].x + blockManager.blocks[i].dimension - collisionXPadding, blockManager.blocks[i].y + 5, character.x + (character.width / 3), character.y + character.height, character.width / 3, 5)) {
             character.y = blockManager.blocks[i].y;
             character.hitGround(blockManager.blocks[i].y);
             console.log("GROUND!");
             //rect(character.x + (character.width/3), character.y + character.height, character.width/3, 10);
         }
         var collisionYPadding = 5;
-        if(collideLineRect(blockManager.blocks[i].x, blockManager.blocks[i].y+collisionYPadding, blockManager.blocks[i].x, blockManager.blocks[i].y + blockManager.blocks[i].dimension - collisionYPadding, character.x + (character.width/1.5), character.y + character.height/1.5, 5, character.height/3) && (character.direction == "RIGHT" || character.direction == "STOP")){
+        if (collideLineRect(blockManager.blocks[i].x, blockManager.blocks[i].y + collisionYPadding, blockManager.blocks[i].x, blockManager.blocks[i].y + blockManager.blocks[i].dimension - collisionYPadding, character.x + (character.width / 1.5), character.y + character.height / 1.5, 5, character.height / 3) && (character.direction == "RIGHT" || character.direction == "STOP")) {
             //character.x = blocks[i].x - (character.width/2);
             character.hitLeftWall();
             //rect(character.x + (character.width/1.5), character.y + character.height/1.5, 5, character.height/3);
         }
-        if(collideLineRect(blockManager.blocks[i].x + blockManager.blocks[i].dimension, blockManager.blocks[i].y + collisionYPadding, blockManager.blocks[i].x + blockManager.blocks[i].dimension, blockManager.blocks[i].y + blockManager.blocks[i].dimension - collisionYPadding, character.x + (character.width/3), character.y + character.height/1.5, 5, character.height/3) && (character.direction == "LEFT" || character.direction == "STOP")){
+        if (collideLineRect(blockManager.blocks[i].x + blockManager.blocks[i].dimension, blockManager.blocks[i].y + collisionYPadding, blockManager.blocks[i].x + blockManager.blocks[i].dimension, blockManager.blocks[i].y + blockManager.blocks[i].dimension - collisionYPadding, character.x + (character.width / 3), character.y + character.height / 1.5, 5, character.height / 3) && (character.direction == "LEFT" || character.direction == "STOP")) {
             //character.x = blocks[i].x + blocks[i].dimension + (character.width/2);
             character.hitRightWall();
             //rect(character.x + (character.width/1.5), character.y + character.height/1.5, 5, character.height/3);
@@ -61,10 +74,10 @@ function draw() {
     }
     // TO SHOW CURRENT Y POS - HELPFUL FOR DEBUG
     // console.log("Y POS: "+character.y);
-    if(character.y >= height-100){
+    if (character.y >= height - 100) {
         character.hitGround();
     }
-    if(mouseIsPressed){
+    if (mouseIsPressed) {
         blockManager.addRemoveBlocks();
     }
     character.draw();
@@ -74,6 +87,19 @@ function sideTab(w, h) {
     rect(20, 20, w, h);
 }
 
-function mouseReleased(){
+function mouseReleased() {
     blockManager.setMouseClicked(false);
+}
+
+function keyPressed() {
+    if (keyCode === ENTER && gameState === 'menu') {
+        gameState = 'gameplay';
+        stroke(0);
+        fill(255);
+        strokeWeight(1);
+        textSize(12);
+        textFont('Arial');
+        textAlign(LEFT, BASELINE);
+        textStyle(NORMAL);
+    }
 }
---
+++ code/screens.js
@@ -0,0 +1,30 @@
+function drawMenuScreen() {
+    background(0);
+    let r = sin(frameCount * 0.05) * 127 + 128; 
+    let g = cos(frameCount * 0.03) * 127 + 128;
+    let b = sin(frameCount * 0.07) * 127 + 128;
+
+    stroke(r, g, b);
+    noFill();
+    strokeWeight(r); // added this for the sillies but it seems kinda cool
+    rect(100, 100, width - 200, height - 200);
+
+    noStroke();
+    fill(255, 0, 0);
+    textSize(50);
+    textAlign(CENTER);
+    textFont('Impact');
+    text('BLOCK BOY', width / 2, 150);
+    textSize(30);
+    text('MADE BY ROCKET BROS.', width / 2, 180);
+
+    fill(0, 255, 0);
+    textSize(20);
+    text('How to play:', width / 2, 220);
+    text('Player 1 - move using the arrow keys', width / 2, 245);
+    text('Player 2 - Click the mouse to create blocks for player 1 to stand on:', width / 2, 270);
+    text('Your Mission...', width / 2, 295);
+    text('DON\'T DIE ', width / 2, 320);
+    fill(255, 255, 255);
+    text('Press Enter to begin', width / 2, 380);
+}
